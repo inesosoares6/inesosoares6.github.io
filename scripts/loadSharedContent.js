1f077217b115e91fe3d90a3febdba5cd68851d24
Inês Soares <inesosoares.6@gmail.com>
2025-02-27 15:29:33 +0000

    refactor navbar

--- scripts/loadSharedContent.js
+++ scripts/loadSharedContent.js
@@ -1,5 +1,55 @@
+const pages = ["Work", "Academic", "Travel", "Sports", "Contact"];
+
+const socials = [
+  {
+    icon: "linkedin",
+    link: "https://www.linkedin.com/in/inesosoares6",
+  },
+  {
+    icon: "github",
+    link: "https://github.com/inesosoares6",
+  },
+  {
+    icon: "file-person-fill",
+    link: "https://drive.google.com/drive/folders/19vQcMkU6VvguD7HfsRRtJksrcpaIfRVu?usp=sharing",
+  },
+];
+
+const createNavBarCards = () => {
+  const navBarContainer = document.querySelector(".navbar-nav");
+  const socialsContainer = document.querySelector(".socials");
+
+  pages.forEach((page) => {
+    const cardHTML = `<li class="nav-item">
+          <a class="nav-link" href="./${page.toLowerCase()}.html">${page}</a>
+        </li>`;
+
+    navBarContainer.insertAdjacentHTML("beforeend", cardHTML);
+  });
+
+  socials.forEach((social) => {
+    const cardHTML = `<li class="ms-3">
+          <a
+            class="text-body-secondary"
+            href="${social.link}"
+            target="_blank"
+            rel="noopener noreferrer"
+            ><img
+              src="./assets/icons/${social.icon}.svg"
+              alt="${social.icon} icon"
+              width="20"
+            />
+          </a>
+        </li>`;
+
+    socialsContainer.insertAdjacentHTML("beforeend", cardHTML);
+  });
+};
+
 $(function () {
   $("#navbar-placeholder").load("./shared/navbar.html", () => {
+    createNavBarCards();
+
     const currentPage = window.location.pathname.split("/").pop();
 
     // Match with './' prefix
